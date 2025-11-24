2b64fb15acf40f05ec51f214d51aba3a93b74673
Murilo <160614784+murilogfs@users.noreply.github.com>
2025-11-24 13:09:06 -0300

    Create four.py

---
+++ four.py
@@ -0,0 +1,13 @@
+def media_final(media:float) -> str:
+    if media >= 9.0:
+        return "A"
+    elif media >=7.0:
+        return "B"
+    elif media >= 5.0:
+        return "C"
+    elif media >= 0.0:
+        return "D"
+    else:
+        return"Notas Inválidas"
+  media_final()
+print(f"Média 5.0: Conceito {retorna_conceito(5.0)"})
